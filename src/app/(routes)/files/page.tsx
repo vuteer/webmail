f130917615f15d9cb2bae40322fcba3389f529f4
kesh25 <kinyua.dn@outlook.com>
2025-07-28 19:00:24 +0300

    custom calendar

--- src/app/(routes)/files/page.tsx
+++ src/app/(routes)/files/page.tsx
@@ -1,9 +1,8 @@
 // files page
 import Protected from "../components/protected";
-// import FilesContainer from "./components/container";
 
 const Page = () => {
-  return <Protected title="My Files">{/* <FilesContainer /> */}</Protected>;
+  return <Protected title="🗂️  My Files">{/* <FilesContainer /> */}</Protected>;
 };
 
 export default Page;
